fd5423456274b5a0c9ca690275711c078c5834ba
hearsawho25 <robert@hearsawho.com>
2026-06-20 16:59:22 -0700

    feat(client): router, App, and entry point

---
+++ client/src/routes.tsx
@@ -0,0 +1,100 @@
+import { createBrowserRouter } from 'react-router-dom';
+import ProtectedRoute from './components/ProtectedRoute';
+import GuestRoute from './components/GuestRoute';
+import AccountRoute from './components/AccountRoute';
+import HomePage from './pages/HomePage';
+import LoginPage from './pages/LoginPage';
+import RegisterPage from './pages/RegisterPage';
+import OnboardingPage from './pages/OnboardingPage';
+import SessionExpiredPage from './pages/SessionExpiredPage';
+import MyGardensPage from './pages/MyGardensPage';
+import SeedCataloguePage from './pages/SeedCataloguePage';
+import PlantingGuidePage from './pages/PlantingGuidePage';
+import DataMetricsPage from './pages/DataMetricsPage';
+import AccountPage from './pages/AccountPage';
+import NotFoundPage from './pages/NotFoundPage';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: (
+      <ProtectedRoute requireOnboarding>
+        <HomePage />
+      </ProtectedRoute>
+    ),
+  },
+  {
+    path: '/onboarding',
+    element: (
+      <ProtectedRoute>
+        <OnboardingPage />
+      </ProtectedRoute>
+    ),
+  },
+  {
+    path: '/login',
+    element: (
+      <GuestRoute>
+        <LoginPage />
+      </GuestRoute>
+    ),
+  },
+  {
+    path: '/register',
+    element: (
+      <GuestRoute>
+        <RegisterPage />
+      </GuestRoute>
+    ),
+  },
+  {
+    path: '/session-expired',
+    element: <SessionExpiredPage />,
+  },
+  {
+    path: '/gardens',
+    element: (
+      <ProtectedRoute>
+        <MyGardensPage />
+      </ProtectedRoute>
+    ),
+  },
+  {
+    path: '/catalogue',
+    element: (
+      <ProtectedRoute>
+        <SeedCataloguePage />
+      </ProtectedRoute>
+    ),
+  },
+  {
+    path: '/guide',
+    element: (
+      <ProtectedRoute>
+        <PlantingGuidePage />
+      </ProtectedRoute>
+    ),
+  },
+  {
+    path: '/data',
+    element: (
+      <AccountRoute>
+        <DataMetricsPage />
+      </AccountRoute>
+    ),
+  },
+  {
+    path: '/account',
+    element: (
+      <AccountRoute>
+        <AccountPage />
+      </AccountRoute>
+    ),
+  },
+  {
+    path: '*',
+    element: <NotFoundPage />,
+  },
+]);
+
+export default router;
